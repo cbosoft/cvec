5749ab8c1fcda25d7d35ed15fa5befa0250bffbf
Chris <chrismjboyle@gmail.com>
2019-06-04 10:52:53 +0100

    Added autocorr func

--- cvec_stats.c
+++ cvec_stats.c
@@ -33,7 +33,7 @@ cvec_hist(cvec_float *input, int len, cvec_float **output, cvec_float **bins, in
   for (i = 0; i < len; i++) {
     int bidx = (int)(input[i] / binw);
     if (bidx > (*nbins) || bidx < 0) {
-      fprintf(stderr, "FATAL ERROR! gplt_hist: bin width calc has failed.\n");
+      fprintf(stderr, "FATAL ERROR! cvec_hist: bin width calc has failed.\n");
       exit(1);
     }
     (*output)[bidx] += 1.0;
@@ -43,5 +43,62 @@ cvec_hist(cvec_float *input, int len, cvec_float **output, cvec_float **bins, in
   for (i = 0; i < (*nbins); i++) {
     (*bins)[i] = ((double)i + 0.5)*binw;
   }
+
+}
+
+
+
+
+void
+cvec_autocorr(cvec_float* x, cvec_float *y, int len, cvec_float **res_x, cvec_float **res_y, int *nbins) {
+  // time correlation
+  //
+  // for each data point, for each lag dt, get product, sum:
+  //    tcorr = VxV(l) = int _-inf^inf V(t)xV(t+l) dt
+  // normalise wrt initial
+
+  // for discrete signals, autocorr R is given by:
+  // R = sum_0^N y(n) av(y(n-l))
+
+  if (x == NULL || y == NULL) {
+    fprintf(stderr, "FATAL ERROR! cvec_autocorr: input cannot be NULL\n");
+    exit(1);
+  }
+
+  cvec_float *dx = cvec_diff(x, len);
+  cvec_float mindt = CVEC_FLOAT_MAX, maxdt = -CVEC_FLOAT_MAX;
+
+  for (int i = 0; i < (len-1); i++) {
+    if (dx[i] < mindt)
+      mindt = dx[i];
+    if (dx[i] > maxdt)
+      maxdt = dx[i];
+  }
+  cvec_float binw = mindt * 1.0;
+  free(dx);
+
+  (*nbins) = ceil((x[len-1] - x[0]) / binw);
+
+  (*res_x) = malloc(sizeof(cvec_float)*(*nbins));
+  (*res_y) = cvec_zeros((*nbins));
+
+  // get correlation
+  for (int i = 0; i < len; i++) {
+    for (int j = i+1; j < len; j++) {
+      cvec_float dt = x[j] - x[i];
+      int ibin = (int)(dt/binw);
+      (*res_y)[ibin] += y[i]*y[j];
+    }
+  }
+
+  // fill res_x
+  for (int i = 0; i < (*nbins); i++) {
+    (*res_x)[i] = ((cvec_float)(i + 1)) * binw;
+  }
+
+  // get max and normalize output
+  cvec_float maxv = cvec_max((*res_y), (*nbins));
+  cvec_float divmax(cvec_float v) { return v / maxv; }
+  cvec_apply((*res_y), (*nbins), &divmax);
 
 }
