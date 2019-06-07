9c4283a8f8530bd0151a6573a641b3014430540f
Chris <chrismjboyle@gmail.com>
2019-06-07 09:03:56 +0100

    Improved matrix test

--- mattest.c
+++ mattest.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 #include <cvec.h>
+#include <stdlib.h>
 
-int
-main (int argc, const char** argv)
-{
-  int R = 3, C = 3;
-  cvec_float g2(int r, int c) {
-    return ((cvec_float)((r+1)+(c+1)));
-  }
-  cvec_float g1(int r, int c) {
-    return ((cvec_float)( (r==c)?(1.0):(0.0)   ));
-  }
-  cvec_float **M = cvec_matrix_new(R, C, &g1);
-  cvec_float **N = cvec_matrix_new(R, C, &g2);
-
+void
+test_matrix(cvec_float **M, cvec_uint R, cvec_uint C) {
+  fprintf(stderr, "%ux%x matrix:\n", R, C);
   cvec_print_matrix(M, R, C);
   cvec_float det = cvec_matrix_determinant(M, R, C);
   fprintf(stderr, "Determinant: %f\n", det);
   cvec_float **inv_M = cvec_matrix_invert(M, R, C);
-  cvec_matrix_free(M, R, C);
 
   if (inv_M != NULL) {
     fprintf(stderr, "Inverse:\n");
@@ -27,15 +17,22 @@ main (int argc, const char** argv)
   }
 
   fprintf(stderr, "\n");
+}
 
-  cvec_print_matrix(N, R, C);
-  det = cvec_matrix_determinant(N, R, C);
-  fprintf(stderr, "Determinant: %f\n", det);
-  cvec_float **inv_N = cvec_matrix_invert(N, R, C);
+int
+main (int argc, const char** argv)
+{
+  int R = 5, C = 5;
+  srand(1);
+  cvec_float **M = cvec_matrix_new(R, C, &cvec_matgen_identity);
+  cvec_float **N = cvec_matrix_new(R, C, &cvec_matgen_random);
+  cvec_float **O = cvec_matrix_new(R, C, &cvec_matgen_ones);
+
+  test_matrix(M, R, C);
+  test_matrix(N, R, C);
+  test_matrix(O, R, C);
+
+  cvec_matrix_free(M, R, C);
   cvec_matrix_free(N, R, C);
-  if (inv_N != NULL) {
-    fprintf(stderr, "Inverse:\n");
-    cvec_print_matrix(inv_N, R, C);
-    cvec_matrix_free(inv_N, R, C);
-  }
+  cvec_matrix_free(O, R, C);
 }
