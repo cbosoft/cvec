3515366fb3751b7166c9076005bd3be017327cab
Chris <chrismjboyle@gmail.com>
2019-06-07 08:31:11 +0100

    uint everywhere!

--- cvec.c
+++ cvec.c
@@ -8,12 +8,12 @@
 
 
 cvec_float *
-cvec_linspace(cvec_float from, cvec_float to, int len) {
+cvec_linspace(cvec_float from, cvec_float to, cvec_uint len) {
   cvec_float *rv = malloc(len*sizeof(cvec_float));
   cvec_float step = (to - from) / ((cvec_float)(len - 1));
 
 #pragma omp parallel for
-  for (int i = 0; i < len; i++) {
+  for (cvec_uint i = 0; i < len; i++) {
     rv[i] = (step*((cvec_float)i)) + from;
   }
 
@@ -24,12 +24,12 @@ cvec_linspace(cvec_float from, cvec_float to, int len) {
 
 
 cvec_float *
-cvec_logspace(cvec_float from, cvec_float to, int len) {
+cvec_logspace(cvec_float from, cvec_float to, cvec_uint len) {
   cvec_float lfrom = log(from), lto = log(to);
   cvec_float *rv = cvec_linspace(lfrom, lto, len);
 
 #pragma omp parallel for
-  for (int i  = 0; i < len; i++) {
+  for (cvec_uint i  = 0; i < len; i++) {
     rv[i] = pow(10.0, rv[i]);
   }
 
@@ -40,11 +40,11 @@ cvec_logspace(cvec_float from, cvec_float to, int len) {
 
 
 cvec_float *
-cvec_apply(cvec_float* in, int len, cvec_float (*f)(cvec_float)) {
+cvec_apply(cvec_float* in, cvec_uint len, cvec_float (*f)(cvec_float)) {
   cvec_float *rv = malloc(len*sizeof(cvec_float));
 
 #pragma omp parallel for
-  for (int i = 0; i < len; i++){
+  for (cvec_uint i = 0; i < len; i++){
     rv[i] = f(in[i]);
   }
   return rv;
@@ -62,12 +62,12 @@ cvec_float cvec_exp(cvec_float v1, cvec_float v2) { return pow(v1, v2); }
 
 
 cvec_float *
-cvec_apply2(cvec_float *in1, cvec_float *in2, int len, cvec_float (*f)(cvec_float, cvec_float))
+cvec_apply2(cvec_float *in1, cvec_float *in2, cvec_uint len, cvec_float (*f)(cvec_float, cvec_float))
 {
   cvec_float *rv = malloc(len*sizeof(cvec_float));
 
 #pragma omp parallel for
-  for (int i = 0; i < len; i++){
+  for (cvec_uint i = 0; i < len; i++){
     rv[i] = f(in1[i], in2[i]);
   }
   return rv;
@@ -77,12 +77,12 @@ cvec_apply2(cvec_float *in1, cvec_float *in2, int len, cvec_float (*f)(cvec_floa
 
 
 cvec_float *
-cvec_zeros(int len)
+cvec_zeros(cvec_uint len)
 {
   cvec_float *rv = malloc(len*sizeof(cvec_float));
 
 #pragma omp parallel for
-  for (int i = 0; i < len; i++){
+  for (cvec_uint i = 0; i < len; i++){
     rv[i] = 0.0;
   }
   return rv;
@@ -91,11 +91,11 @@ cvec_zeros(int len)
 
 
 cvec_float *
-cvec_copy(cvec_float *source, int len)
+cvec_copy(cvec_float *source, cvec_uint len)
 {
   cvec_float *rv = malloc(sizeof(cvec_float)*len);
 #pragma omp parallel for
-  for(int i = 0; i < len; i++) {
+  for(cvec_uint i = 0; i < len; i++) {
     rv[i] = source[i];
   }
   return rv;
@@ -104,17 +104,17 @@ cvec_copy(cvec_float *source, int len)
 
 
 cvec_float *
-cvec_cat(cvec_float *source, int len, cvec_float *add, int addlen)
+cvec_cat(cvec_float *source, cvec_uint len, cvec_float *add, cvec_uint addlen)
 {
   cvec_float *rv = malloc(sizeof(cvec_float)*(len+addlen));
 
 #pragma omp parallel for
-  for(int i = 0; i < len; i++) {
+  for(cvec_uint i = 0; i < len; i++) {
     rv[i] = source[i];
   }
 
 #pragma omp parallel for
-  for(int i = len; i < (len+addlen); i++) {
+  for(cvec_uint i = len; i < (len+addlen); i++) {
     rv[i] = add[i-len];
   }
 
@@ -125,9 +125,9 @@ cvec_cat(cvec_float *source, int len, cvec_float *add, int addlen)
 
 
 cvec_float *
-cvec_diff(cvec_float* x, int len) {
+cvec_diff(cvec_float* x, cvec_uint len) {
   cvec_float *rv = malloc(sizeof(cvec_float)*(len-1));
-  int i, j;
+  cvec_uint i, j;
 #pragma omp parallel for
   for (i=0; i < (len-1); i++) {
     j = i+1;
@@ -139,11 +139,11 @@ cvec_diff(cvec_float* x, int len) {
 
 
 cvec_float *
-cvec_slice(cvec_float *x, int len, int start, int stop, int skip)
+cvec_slice(cvec_float *x, cvec_uint len, cvec_uint start, cvec_uint stop, cvec_uint skip)
 {
-  int olen = (stop - start) / skip;
+  cvec_uint olen = (stop - start) / skip;
   cvec_float *rv = malloc(sizeof(cvec_float)*olen);
-  for (int i = start, j = 0; i < stop; i += skip, j++) {
+  for (cvec_uint i = start, j = 0; i < stop; i += skip, j++) {
     rv[j] = x[i];
   }
   return rv;
@@ -153,14 +153,14 @@ cvec_slice(cvec_float *x, int len, int start, int stop, int skip)
 
 
 cvec_float
-cvec_get_sumse(cvec_float *x, cvec_float *y, int len, cvec_float *coefs, int ncoefs)
+cvec_get_sumse(cvec_float *x, cvec_float *y, cvec_uint len, cvec_float *coefs, cvec_uint ncoefs)
 {
   cvec_float calc_se(cvec_float o, cvec_float g) { 
     return pow(o - g, 2.0); 
   }
   cvec_float calcfit(cvec_float xi) { 
     cvec_float rv = 0.0; 
-    for (int i = 0; i < ncoefs; i++) 
+    for (cvec_uint i = 0; i < ncoefs; i++) 
       rv += coefs[i] * pow(xi, (double)i); 
     return rv; 
   }
@@ -176,37 +176,21 @@ cvec_get_sumse(cvec_float *x, cvec_float *y, int len, cvec_float *coefs, int nco
 
 
 cvec_float *
-cvec_polyfit(cvec_float *X, cvec_float *Y, int len, int degree)
+cvec_polyfit(cvec_float *X, cvec_float *Y, cvec_uint len, cvec_uint degree)
 {
   // https://github.com/natedomin/polyfit
-  int maxdeg = 5;
+  cvec_uint maxdeg = 5;
   
-  // cvec_float B[maxOrder+1] = {0.0f};
-  // cvec_float P[((maxOrder+1) * 2)+1] = {0.0f};
-  // cvec_float A[(maxOrder + 1)*2*(maxOrder + 1)] = {0.0f};
   cvec_float *B = calloc(maxdeg+1, sizeof(cvec_float));
   cvec_float *P = calloc(((maxdeg+1)*2)+1, sizeof(cvec_float));
   cvec_float *A = calloc((maxdeg+1)*2*(maxdeg+1), sizeof(cvec_float));
 
   double x, y, powx;
 
-  int i, j, k;
+  cvec_uint i, j, k;
 
-  // Verify initial conditions....
-  // ----------------------------------
-
-  // This method requires that the countOfElements > 
-  // (degree+1) 
-  if (len <= degree)
+  if (len <= degree || degree > maxdeg)
       return NULL;
-
-  // This method has imposed an arbitrary bound of
-  // degree <= maxOrder.  Increase maxOrder if necessary.
-  if (degree > maxdeg)
-      return NULL;
-
-  // Begin Code...
-  // ----------------------------------
 
   // Identify the column vector
   for (i = 0; i < len; i++) {
@@ -290,16 +274,16 @@ cvec_polyfit(cvec_float *X, cvec_float *Y, int len, int degree)
 
 
 cvec_float *
-cvec_linearfit(cvec_float *x, cvec_float *y, int len)
+cvec_linearfit(cvec_float *x, cvec_float *y, cvec_uint len)
 {
   cvec_float midx = cvec_average(x, len);
   cvec_float midy = cvec_average(y, len);
 
-  cvec_float get_int(cvec_float m) { return midy - (m*midx); }
+  cvec_float get_cvec_uint(cvec_float m) { return midy - (m*midx); }
 
-  cvec_float get_sumse(cvec_float *x, cvec_float *y, int len, cvec_float gradient) {
-    cvec_float interrupt = get_int(gradient);
-    cvec_float applyfit(cvec_float v) { return interrupt+(gradient*v); }
+  cvec_float get_sumse(cvec_float *x, cvec_float *y, cvec_uint len, cvec_float gradient) {
+    cvec_float cvec_uinterrupt = get_cvec_uint(gradient);
+    cvec_float applyfit(cvec_float v) { return cvec_uinterrupt+(gradient*v); }
     cvec_float *fity = cvec_apply(x, len, &applyfit);
     cvec_float getse(cvec_float y1, cvec_float y2) {
       return pow(y1 - y2, 2.0);
@@ -312,7 +296,7 @@ cvec_linearfit(cvec_float *x, cvec_float *y, int len)
   }
 
   cvec_float change = 0.1, m = 1.0;
-  for (int i = 0; i < len+100; i++) {
+  for (cvec_uint i = 0; i < len+100; i++) {
     cvec_float err1 = get_sumse(x, y, len, m);
     m += change;
     cvec_float err2 = get_sumse(x, y, len, m);
@@ -325,7 +309,7 @@ cvec_linearfit(cvec_float *x, cvec_float *y, int len)
     }
   }
   cvec_float *coefs = malloc(sizeof(cvec_float)*2);
-  coefs[0] = get_int(m);
+  coefs[0] = get_cvec_uint(m);
   coefs[1] = m;
   return coefs;
 }
@@ -334,10 +318,10 @@ cvec_linearfit(cvec_float *x, cvec_float *y, int len)
 
 
 cvec_float
-cvec_max(cvec_float* x, int len)
+cvec_max(cvec_float* x, cvec_uint len)
 {
   cvec_float rv = -CVEC_FLOAT_MAX;
-  for (int i = 0; i < len; i++) {
+  for (cvec_uint i = 0; i < len; i++) {
     if (x[i] > rv) {
       rv = x[i];
     }
@@ -349,9 +333,9 @@ cvec_max(cvec_float* x, int len)
 
 
 cvec_float
-cvec_min(cvec_float* x, int len) {
+cvec_min(cvec_float* x, cvec_uint len) {
   cvec_float rv = CVEC_FLOAT_MAX;
-  for (int i = 0; i < len; i++) {
+  for (cvec_uint i = 0; i < len; i++) {
     if (x[i] < rv) {
       rv = x[i];
     }
@@ -363,7 +347,7 @@ cvec_min(cvec_float* x, int len) {
 
 
 cvec_float
-cvec_average(cvec_float* in, int len) {
+cvec_average(cvec_float* in, cvec_uint len) {
   cvec_float sum = cvec_sum(in, len);
   return sum / ((cvec_float)len);
 }
@@ -372,9 +356,9 @@ cvec_average(cvec_float* in, int len) {
 
 
 cvec_float
-cvec_sum(cvec_float* in, int len) {
+cvec_sum(cvec_float* in, cvec_uint len) {
   cvec_float sum = 0.0;
-  for (int i = 0; i < len; i++) {
+  for (cvec_uint i = 0; i < len; i++) {
     sum += in[i];
   }
   return sum;
