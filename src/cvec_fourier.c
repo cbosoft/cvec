597b7d601eeee176931342c8efd354825a4c7c7d
Chris <chrismjboyle@gmail.com>
2019-06-11 14:44:56 +0100

    Tidying

--- src/cvec_fourier.c
+++ src/cvec_fourier.c
@@ -36,12 +36,13 @@ cvec_fft(cvec_float *in, cvec_uint in_len) {
 cvec_float *
 cvec_freq(cvec_uint len, cvec_float dt) {
   cvec_float *freq = malloc(len*sizeof(cvec_float));
-  double nyq_f = 1/(2.0*dt);
-  cvec_uint hlen = (len%2==0)?len/2:(len+1)/2; // unsure about this
+  cvec_float nyq_f = 1/(2.0*dt);
+  cvec_uint hlen = (len%2==0)?len:(len+1); // unsure about this
+  hlen /= 2;
   for (cvec_uint in = 0; in < hlen; in++) {
     cvec_float n = (cvec_float)in;
     freq[in] = (n*nyq_f) / hlen;
-    freq[len - in - 1] = -1.0 * ((n*nyq_f) / hlen);
+    freq[len - in - 1] = (-1.0*n*nyq_f) / hlen;
   }
   return freq;
 }
