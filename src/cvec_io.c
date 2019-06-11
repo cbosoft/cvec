afbb75d89337d2593b546c5c4e009de5c04065db
Chris <chrismjboyle@gmail.com>
2019-06-11 16:00:50 +0100

    Made more safer

--- src/cvec_io.c
+++ src/cvec_io.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 #include "cvec.h"
+
+FILE *cvec_safe_fopen(const char *path, const char *t) {
+  FILE *rv = fopen(path, t);
+  if (rv == NULL) {
+    fprintf(stderr, "\033[31mFATAL ERROR!\033[0m cvec_safe_fopen: (%d) %s\n", errno, strerror(errno));
+    exit(1);
+  }
+  return rv;
+}
+
+
+
+
 
 void
 cvec_write_csv(cvec_float **data, cvec_uint ncols, cvec_uint nlines, const char *csv_path)
 {
-  FILE *csvf = fopen(csv_path, "w");
+  FILE *csvf = cvec_safe_fopen(csv_path, "w");
 
   for (cvec_uint l = 0; l < nlines; l++) {
     for (cvec_uint c = 0; c < ncols; c++) {
@@ -22,7 +38,7 @@ cvec_write_csv(cvec_float **data, cvec_uint ncols, cvec_uint nlines, const char 
 void
 cvec_write_csv_xy(cvec_float *x, cvec_float *y, cvec_uint len, const char *csv_path)
 {
-  FILE *csvf = fopen(csv_path, "w");
+  FILE *csvf = cvec_safe_fopen(csv_path, "w");
 
   for (cvec_uint l = 0; l < len; l++) {
     fprintf(csvf, "%f,%f\n", x[l], y[l]);
