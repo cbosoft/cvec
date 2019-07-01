cbd42dd870a4607052b28b1c86e68a0ff29685f6
Chris <chrismjboyle@gmail.com>
2019-07-01 09:04:15 +0100

    Copes with different lengths of column in csv output

--- src/cvec_io.c
+++ src/cvec_io.c
@@ -18,13 +18,20 @@ FILE *cvec_safe_fopen(const char *path, const char *t) {
 
 
 void
-cvec_write_csv(cvec_float **data, cvec_uint ncols, cvec_uint nlines, const char *csv_path)
+cvec_write_csv(cvec_float **data, cvec_uint ncols, cvec_uint *nlines, const char *csv_path)
 {
   FILE *csvf = cvec_safe_fopen(csv_path, "w");
 
-  for (cvec_uint l = 0; l < nlines; l++) {
+  cvec_uint maxlen = 0;
+  for (cvec_uint i = 0; i < ncols; i++) {
+    if (nlines[i] > maxlen)
+      maxlen = nlines[i];
+  }
+  
+  for (cvec_uint l = 0; l < maxlen; l++) {
     for (cvec_uint c = 0; c < ncols; c++) {
-      fprintf(csvf, "%f,", data[c][l]);
+      if (nlines[c] < l)
+        fprintf(csvf, "%f,", data[c][l]);
     }
     fprintf(csvf, "\n");
   }
