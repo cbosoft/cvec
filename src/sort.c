ce8a815035c7d0490d17960c761738c5cbe9732d
Chris <chrismjboyle@gmail.com>
2019-07-08 13:18:47 +0100

    Made returns optional

--- src/sort.c
+++ src/sort.c
@@ -19,31 +19,40 @@ bool cvec_in_order(cvec_float* in, cvec_uint len)
 
 void cvec_bubble_sort(cvec_float *unsorted, cvec_uint len, cvec_uint **sorted_indices, cvec_float **sorted_values)
 {
-  cvec_float *svs = cvec_copy(unsorted, len);
-  cvec_uint *sids = malloc(len*sizeof(cvec_uint));
-  for (cvec_uint i = 0; i < len; i++) sids[i] = i;
+  cvec_uint ret_ids = 0, ret_vals = 0;
+
+  if (sorted_indices != NULL) {
+    (*sorted_indices) = cvec_uint_linspace(0, len, len);
+    ret_ids = 1;
+  }
+
+  if (sorted_values != NULL) {
+    (*sorted_values) = cvec_copy(unsorted, len);
+    ret_vals = 1;
+  }
 
   cvec_uint changed = 0;
   do {
     changed = 0;
     for (cvec_uint i = 0, j = 1; j < len; i++, j++) {
-      if (svs[j] < svs[i]) {
-
-        cvec_float temp = svs[j];
-        svs[j] = svs[i];
-        svs[i] = temp;
-
-        cvec_int temp2 = sids[j];
-        sids[j] = sids[i];
-        sids[i] = temp2;
+      if ((*sorted_values)[j] < (*sorted_values)[i]) {
+        
+        if (ret_vals) {
+          cvec_float temp = (*sorted_values)[j];
+          (*sorted_values)[j] = (*sorted_values)[i];
+          (*sorted_values)[i] = temp;
+        }
+        
+        if (ret_ids) {
+          cvec_int temp = (*sorted_indices)[j];
+          (*sorted_indices)[j] = (*sorted_indices)[i];
+          (*sorted_indices)[i] = temp;
+        }
 
         changed = 1;
       }
     }
   } while (changed);
-
-  (*sorted_indices) = sids;
-  (*sorted_values) = svs;
 
 }
 
