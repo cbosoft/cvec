50f786bf1d0410b59814ba5d31d5b5d65a8056c9
Chris <chrismjboyle@gmail.com>
2019-07-16 08:50:57 +0100

    Flush after progressbar write

--- src/display.c
+++ src/display.c
@@ -17,11 +17,12 @@ void progressbar(const char* message, cvec_float progress)
       cvec_uint barl = width_total - strlen(message) - 2 - strlen(percstr) - 2;
       cvec_uint pind = ((cvec_uint)(progress*barl)) / 100;
       
-      fprintf(stdout, "%s ▕", message);
+      fprintf(stdout, "\r%s ▕", message);
       for (cvec_uint i = 0; i < pind; i++) fprintf(stdout, "▉");
       for (cvec_uint i = pind; i < barl; i++) fprintf(stdout, " ");
 
-      fprintf(stdout, "▏ %s\r", percstr);
+      fprintf(stdout, "▏ %s", percstr);
+      fflush(stdout);
 
 }
 
